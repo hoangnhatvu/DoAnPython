f92a5d62bbcf5a8056f0b32e05dd3fa32c049e0a
Khowfsix <lyhongphat261202@gmail.com>
2022-04-08 13:50:29 +0700

    Ly Hong Phat

--- store/views/signup.py
+++ store/views/signup.py
@@ -46,23 +46,19 @@ class Signup (View):
     def validateCustomer(self, customer):
         error_message = None
         if (not customer.first_name):
-            error_message = "Please Enter your First Name !!"
-        elif len (customer.first_name) < 3:
-            error_message = 'First Name must be 3 char long or more'
-        elif not customer.last_name:
-            error_message = 'Please Enter your Last Name'
-        elif len (customer.last_name) < 3:
-            error_message = 'Last Name must be 3 char long or more'
-        elif not customer.phone:
-            error_message = 'Enter your Phone Number'
-        elif len (customer.phone) < 10:
-            error_message = 'Phone Number must be 10 char Long'
+            error_message = "Vui lòng nhập Họ của bạn."
+        elif (not customer.last_name):
+            error_message = 'Vui lòng nhập Tên của bạn.'
+        elif (not customer.phone):
+            error_message = 'Vui lòng nhập số điện thoại.'
+        elif not 10 <= len(customer.phone) <= 15:
+            error_message = 'Số điện thoại không hợp lệ.'
         elif len (customer.password) < 5:
-            error_message = 'Password must be 5 char long'
+            error_message = 'Mật khẩu phải có độ dài hơn 5 ký tự'
         elif len (customer.email) < 5:
-            error_message = 'Email must be 5 char long'
+            error_message = 'Địa chỉ Email không hợp lệ.'
         elif customer.isExists ():
-            error_message = 'Email Address Already Registered..'
+            error_message = 'Tài khoản Email này đã tồn tại !!!'
         # saving
 
         return error_message
